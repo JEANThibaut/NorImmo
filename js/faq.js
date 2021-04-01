4e33eb396c50cfd3495c8fabcad50b5c529ebaee
AlexisFioreCode <alexis.fiorentino.pro@gmail.com>
2021-04-01 15:39:09 +0200

    added some css and animation

--- js/faq.js
+++ js/faq.js
@@ -1,19 +1,25 @@
 //récupérer la classe accordion
  let accs = document.getElementsByClassName("accordion");
-    
+
 for ( let acc of accs) {;
     acc.onclick = function() {
+        this.classList.toggle("active");
         //récupérer le prochain élément après le bouton
         let answer = this.nextElementSibling;
-             // ajouter display block ou none / enlever le display 
+             // add display block or none / remove display 
         if (answer.classList.contains("d-block")) {
         answer.classList.remove("d-block");
         answer.classList.add("d-none");
-        } else if(answer.classList.contains("d-none")){
-            answer.classList.remove("d-none"); 
-                answer.classList.add("d-block");    
         } 
+        else if(answer.classList.contains("d-none")){
+        answer.classList.remove("d-none"); 
+        answer.classList.add("d-block");   
+        // aadd transition        
+        } if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+        } 
+        else {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        };
     }
 }
-
-
