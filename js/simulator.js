60c65cd652d53bafc3e88362f4f70139b28c5f3b
JEANThibaut <thibaut-jean@live.fr>
2021-04-01 15:58:27 +0200

    New Header

--- js/simulator.js
+++ js/simulator.js
@@ -1,27 +1,31 @@
 
-//Récupérer la valeur de l'élement HTML user_surface
-// let user_surface= document.getElementById("user_surface").value;
-// console.log(user_surface);
-
 // Fonction calcul du prix total
 function Price(){
     //Récupérer la valeur de l'élement HTML user_surface
-    let userSurface= document.getElementById("user_surface").value;
-    console.log(userSurface);
+    let userSurface=document.getElementById("user_surface").value;
+    
     //Calcul du prix de la surface
     let surfacePrice=userSurface * 1000;
-    console.log(surfacePrice);
+    
 
     //Récupérer la valeur de l'élement HTML user_chambers
-    let userChambers= document.getElementById("user_chambers").value;
-    console.log(userChambers);
+    let userChambers=document.getElementById("user_chambers").value;
+    
     // Calcul du prix des chambres
     let chambersPrice=userChambers* 10000;
-    console.log(chambersPrice);
+    
     //Calcul du prix de la surface et des chambres
     let priceWithoutPourcent = surfacePrice + chambersPrice;
-    console.log(priceWithoutPourcent);
+    
+    //Choix du matériaux
+    let userChoice = document.getElementById("selected").value;
+   
+
+    let total= (priceWithoutPourcent*userChoice)+priceWithoutPourcent;
+    console.log("total "+total)
+
 }
+
 
 
 
